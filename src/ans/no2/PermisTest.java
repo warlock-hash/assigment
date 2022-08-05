19a1fa8a1990564d2bf8622f6b4607d64aac765b
usman.arain <usman.arain@avanzasolutions.com>
2022-08-06 02:56:10 +0500

    ans 2 solved update

--- src/ans/no2/PermisTest.java
+++ src/ans/no2/PermisTest.java
@@ -12,14 +12,16 @@ public class PermisTest {
 
         try
         {
-            Scanner scObj = new Scanner (new File("input.txt"));
+            // ye lines mujhe is liye change karni parhi qk humari ye class packge use karhe
+            // agar packge nhi hoga to ye code exactly chale ga
+            Scanner scObj = new Scanner (new File("src/ans/no2/input.txt"));
 
             int no1 = scObj.nextInt();
             int no2 = scObj.nextInt();
             System.out.println("The two nos are : " + no1 + ", " + no2);
 
             // Write to a File
-            Formatter outObj = new Formatter(new File("output.txt"));
+            Formatter outObj = new Formatter(new File("src/ans/no2/output.txt"));
             int totalSum = no1 + no2;
             System.out.println("The total sum value is " + totalSum);
             outObj.format("%d", totalSum);
