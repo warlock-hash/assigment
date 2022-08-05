d7c66729b78e9f85a1545bc517b0bcc8f405ebff
usman.arain <usman.arain@avanzasolutions.com>
2022-08-06 02:01:11 +0500

    ans 1 solved

---
+++ src/ans/no1/ArrArgs.java
@@ -0,0 +1,9 @@
+package ans.no1;
+
+public class ArrArgs {
+    public static void main (String args[]) {
+        for (int k = 0; k < args.length; k++) {
+            System.out.println("Value of input is" + k + "and arguments " + args[k]);
+        }
+    }
+}
